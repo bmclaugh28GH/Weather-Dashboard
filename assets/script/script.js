683320c1be789112500a74febc85efe93a35c7d3
BrianMcL <bmclaugh28@yahoo.com>
2019-12-12 16:48:36 -0500

    replaced 5 day, 3 hour block AJAX call with 16 day, one block per day call. Added values to 5-day forecast. Added icons to both. Added UV index AJAX call, attempted to change UV Index bg color based on the value. I need to accommodate: 1) city not found, and 2) storing new city entries in localStorage

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,14 +10,49 @@ var cityCountryElem = $("#cityCountry");
 var newCity={}; 
 
 var cityElem = $("#city");
+var day0IconElem = $("#day0Icon"); 
 var day0TempElem = $("#day0Temp"); 
 var day0HumidityElem = $("#day0Humidity"); 
 var day0WindSpeedElem = $("#day0WindSpeed"); 
 var day0UVIndexElem = $("#day0UVIndex"); 
 
+var lat = 0; 
+var lon = 0; 
+
 // **********************************************
 // functions
 // **********************************************
+
+// **********************************************
+// get UV index 
+// **********************************************
+function getUVIndex (){
+
+   var queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat="
+      + lat + "&lon=" + lon + "&appid=" + APIKey;
+
+   console.log('uv');
+   //alert ('lat ' + lat + ' lon ' + lon); 
+   console.log (queryURL); 
+
+   $.ajax({
+      url: queryURL,
+      method: "GET"
+   }).then(function(response) {
+
+      day0UVIndexElem.text(response.value);
+      if (parseFloat(response.value) <= 4){
+         day0UVIndexElem.removeClass("safe warning danger").addClass("safe"); 
+      }
+      else if (parseFloat(response.value) > 4 && parseFloat(response.value) <= 7){
+         day0UVIndexElem.removeClass("safe warning danger").addClass("warning"); 
+      }
+      else if (parseFloat(response.value) > 7){
+         day0UVIndexElem.removeClass("safe warning danger").addClass("danger"); 
+      }
+   })
+
+}; // getUVIndex 
 
 // **********************************************
 // get current weather 
@@ -27,57 +62,79 @@ function getCurrentWeather (city, country){
    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
       + city + "," + country + "&units=imperial&appid=" + APIKey;
 
-   console.log (queryURL); 
+   //console.log (queryURL); 
 
    $.ajax({
       url: queryURL,
       method: "GET"
    }).then(function(response) {
 
-      //alert ("base " + response.base); 
+      day0IconElem.attr("src",  "http://openweathermap.org/img/wn/"
+            + response.weather[0].icon 
+            + "@2x.png");
+      day0TempElem.text(Math.round(response.main.temp));  
+      day0HumidityElem.text(Math.round(response.main.humidity));  
+      day0WindSpeedElem.text(Math.round(response.wind.speed));  
 
-      day0TempElem.text(response.main.temp);  
-      day0HumidityElem.text(response.main.humidity);  
-      day0WindSpeedElem.text(response.wind.speed);  
-      day0UVIndexElem.text('????'); 
+      lat = response.coord.lat;
+      lon = response.coord.lon; 
+      getUVIndex();
 
    })
-}; 
+}; //getCurrentWeather
 
 // **********************************************
 // get 5 day forecast 
+// 191212: finally took a close look at this. The "5 day" forecast I was using is 5 days, IN 3 HOUR BLOCKS.
+//    AND the min/max temps in there aren't for the entire day. Try the 16 day forecast, restrict to 6 days, and use
+//    cells 1 thru 5. (0 = today)
 // **********************************************
 function getFiveDayForecast (city, country){
 
-   var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q="
-      + city + "," + country + "&mode=json&units=imperial&appid=" + APIKey;
+   var queryURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q="
+      + city + "," + country + "&mode=json&units=imperial&cnt=6&appid=" + APIKey;
 
-   console.log (queryURL); 
+   //console.log (queryURL); 
 
+   var dayElem;
    $.ajax({
       url: queryURL,
       method: "GET"
    }).then(function(response) {
 
-      //alert('cod' + response.cod); 
+      var responseList = response.list; 
+      for (i=1; i<=5; i++){
 
-      alert('temp ' + response.list[0].main.temp);
+         if (i==1){
+            //alert('icon ' + responseList[i].weather[0].icon);
+         }
+         dayElem = $("#day"+i+"Icon");
+         dayElem.attr("src", "http://openweathermap.org/img/wn/"
+            + responseList[i].weather[0].icon 
+            + "@2x.png");
+         dayElem = $("#day"+i+"Temp"); 
+         //console.log(dayElem); 
+         dayElem.text(Math.round(responseList[i].temp.max));
+         dayElem = $("#day"+i+"Humidity"); 
+         dayElem.text(Math.round(responseList[i].humidity));
+
+      }
    })
-}; 
+}; //getFiveDayForecast
 
 // **********************************************
 // getCity 
 // **********************************************
 function getCity (str){
    return str.slice(0, str.indexOf(',')).trim (); 
-}; 
+}; //getCity
 
 // **********************************************
 // getCountry 
 // **********************************************
 function getCountry (str){
    return str.substr(str.indexOf(',') + 1).trim ();
-}; 
+}; //getCountry
 
 // **********************************************
 // loadCities
